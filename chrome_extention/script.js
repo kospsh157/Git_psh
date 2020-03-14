d29ef2b8b9cb6d5d8b559d35deff15d1f936bca5
kospsh157 <kospsh157@gmail.com>
2020-03-14 23:04:18 +0900

    요청 TEST

--- chrome_extention/script.js
+++ chrome_extention/script.js
@@ -1,1 +1,13 @@
-alert("hello")
+httpRequest.open("GET", "https://www.ted.com/talks/susan_david_the_gift_and_power_of_emotional_courage/transcript?language=ko", true);
+
+httpRequest.send();
+
+if (httpRequest.readyState == XMLHttpRequest.DONE && httpRequest.status == 200 ) {
+
+    alert("요청 성공");
+    alert(httpRequest.responseText);
+
+}else{
+    alert("요청 실패");
+}
+
