8b61efe059b5cc8ec56c69e4c1cc98be5cb67df2
kospsh157 <kospsh157@gmail.com>
2020-03-15 21:20:47 +0900

    요청 성공

--- chrome_extention/script.js
+++ chrome_extention/script.js
@@ -1,12 +1,15 @@
-// var xhr = new XMLHttpRequest();
-// xhr.open("GET", "https://www.ted.com/talks/susan_david_the_gift_and_power_of_emotional_courage/transcript?language=ko", true);
-// xhr.send();
+var xhr = new XMLHttpRequest();
+console.log(xhr);
 
-// xhr.onload = function(){
-//     if(xhr.status == 200){
-//         alert(xhr.responseText);
-//     }
-// }
+
+xhr.open("GET", "https://www.ted.com/talks/susan_david_the_gift_and_power_of_emotional_courage/transcript?language=ko", true);
+xhr.send();
+
+xhr.onload = function(){
+    if(xhr.status == 200){
+        alert(xhr.responseText);
+    }
+}
 
 
 alert("test");
