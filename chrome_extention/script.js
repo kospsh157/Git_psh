adf01fa820e3478c27d13e1510169fa8489fb57b
kospsh157 <kospsh157@gmail.com>
2020-03-26 16:24:05 +0900

    테드 확장프로그램 만들기

--- chrome_extention/script.js
+++ chrome_extention/script.js
@@ -1,16 +1,15 @@
-var xhr = new XMLHttpRequest();
-console.log(xhr);
+var xhr = new XMLHttpRequest()
+console.log(xhr)
 
-
-xhr.open("GET", "https://www.ted.com/talks/susan_david_the_gift_and_power_of_emotional_courage/transcript?language=ko", true);
-xhr.send();
-
-xhr.onload = function(){
-    if(xhr.status == 200){
-        alert(xhr.responseText);
-    }
+xhr.onload = function() {
+  if (xhr.status == 200) {
+    alert(xhr.responseText)
+  }
 }
 
-
-alert("test");
-
+xhr.open(
+  "GET",
+  "https://www.ted.com/talks/susan_david_the_gift_and_power_of_emotional_courage/transcript?language=ko",
+  true
+)
+xhr.send()
