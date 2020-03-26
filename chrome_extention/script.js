44f17250b72b0c0da142993421c843985ff23aa2
kospsh157 <kospsh157@gmail.com>
2020-03-26 18:26:07 +0900

    storage 사용용

--- chrome_extention/script.js
+++ chrome_extention/script.js
@@ -1,25 +1,37 @@
-var xhr = new XMLHttpRequest()
+chrome.tabs.excuteScript(
+  {
+    code: "document.getElementsByClassName('m-b:7 p-l:.5')"
+  },
+  function(result) {
+    alert(result)
+  }
+)
 
+var xhr = new XMLHttpRequest()
+//현재 url 주소가져오기
+//window.location.protocol
+//window.location.hostname
+//window.location.pathname
+console.log("일단 이거는 뜨자")
 
 xhr.onload = function() {
   if (xhr.status == 200) {
-    alert(xhr.responseText)
+    //alert(xhr.responseText)
+    console.log("서버 요청 반응 했음")
   }
 }
 
-var url = window.location.href;
-console.log(url);
-
+var url = window.location.href
+console.log(url)
+alert(url)
+var processURL = null
 
 xhr.open(
   "GET",
-  "https://www.ted.com/talks/susan_david_the_gift_and_power_of_emotional_courage/transcript?language=ko",
+  processURL,
+  //"https://www.ted.com/talks/susan_david_the_gift_and_power_of_emotional_courage/transcript?language=ko",
   true
 )
 xhr.send()
 
-//https://www.ted.com/talks/발표자_비디오제목/transcript?language=ko 
-
-
-
-
+//https://www.ted.com/talks/발표자_비디오제목/transcript?language=ko
