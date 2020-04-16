1b7e2e700bf4d61c12dd892ffdac9d59dfe6b7e0
kospsh157 <kospsh157@gmail.com>
2020-04-17 00:21:15 +0900

    리액트 학습2

--- React-Native/AwesomeProject/App.js
+++ React-Native/AwesomeProject/App.js
@@ -1,19 +1,29 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { Alert } from "react-native"
+import Loading from "./Loading"
+import * as Location from "expo-location"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hi everyone!</Text>
-    </View>
-  )
+export default class extends React.Component {
+  // 리액트 컴포넌트 함수를 상속
+  state = {
+    isLoading: true,
+  }
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync()
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync() // es6 문법
+      this.setState({ isLoading: false })
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad")
+    }
+  }
+  componentDidMount() {
+    this.getLocation()
+  }
+  render() {
+    const { isLoading } = this.state
+    return isLoading ? <Loading /> : null
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
