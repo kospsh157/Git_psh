c3d8e9528333449585263eaf692f82e37db825a8
kospsh157 <kospsh157@gmail.com>
2020-04-25 13:25:03 +0900

    리액트 네이티브

--- React-Native/AwesomeProject/App.js
+++ React-Native/AwesomeProject/App.js
@@ -2,28 +2,54 @@ import React from "react"
 import { Alert } from "react-native"
 import Loading from "./Loading"
 import * as Location from "expo-location"
+import axios from "axios"
+import Weather from "./Weather"
 
+const API_KEY = "d5745f1dbd21511762a243f344eaebf7"
 export default class extends React.Component {
   // 리액트 컴포넌트 함수를 상속
   state = {
     isLoading: true,
   }
+
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    )
+
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    })
+  }
+
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync()
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync() // es6 문법
-      this.setState({ isLoading: false })
+      this.getWeather(latitude, longitude)
     } catch (error) {
       Alert.alert("Can't find you", "So sad")
     }
   }
   componentDidMount() {
+    // 컴포넌트의 DOM이 마운트되고 나서 실행되는 함수  //외부 라이브러리 호출이나, 아작스 요청 같은 것들
     this.getLocation()
   }
   render() {
-    const { isLoading } = this.state
-    return isLoading ? <Loading /> : null
+    const { isLoading, temp, condition } = this.state
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    )
   }
 }
---
+++ React-Native/AwesomeProject/Weather.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { View, Text, StyleSheet } from "react-native"
+import PropTypes from "prop-types"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+export default function Weather({ temp }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          size={90}
+          name="weather-lightning-rainy"
+        ></MaterialCommunityIcons>
+        <Text style={styles.text}>{temp}</Text>
+      </View>
+      <View style={styles.halfContainer}></View>
+    </View>
+  )
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust",
+  ]).isRequired,
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 36,
+  },
+})
