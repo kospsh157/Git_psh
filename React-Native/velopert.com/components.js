4cbb8de8f45d269f9c56dac454bca7d8b5469ce9
psh <kospsh157@gmail.com>
2020-05-25 14:25:46 +0900

    커밋 확인

---
+++ React-Native/velopert.com/components.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MyName extends Component {
+    render(){
+        return (
+            <div>
+                하이요!
+            </div>
+        )
+    }
+}
+
+export default MyName;
