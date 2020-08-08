ba03908b2190d17aec9eeec9e8aadb561f147345
unknown <kospsh157@gmail.com>
2020-08-08 22:25:02 +0900

    정기커밋

---
+++ naver-maps-api/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps';
+
+function App() {
+  const navermaps = window.naver.maps;
+  return (
+    <RenderAfterNavermapsLoaded
+      ncpClientId={'97uime5b4b'} // 자신의 네이버 계정에서 발급받은 Client ID
+      error={<p>Maps Load Error</p>}
+      loading={<p>Maps Loading...</p>}
+    >
+
+      <NaverMap
+      mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+      style={{
+        width: '100%', // 네이버지도 가로 길이
+        height: '85vh' // 네이버지도 세로 길이
+      }}
+      defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
+      defaultZoom={13} // 지도 초기 확대 배율
+      >
+
+
+      <Marker
+        key={1}
+        position={new navermaps.LatLng(37.551229, 126.988205)}
+        animation={2}
+        onClick={() => {alert('여기는 N서울타워입니다.');}}  // 마커를 클릭했을 때
+      />
+
+      </NaverMap>
+
+      
+      
+    
+
+    </RenderAfterNavermapsLoaded>
+  );
+}
+
+export default App;
