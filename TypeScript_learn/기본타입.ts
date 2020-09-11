4f15b7df6a5c4de1da1aacb1374a71a87c6b7b8f
kospsh157 <31397158+kospsh157@users.noreply.github.com>
2020-09-11 23:04:19 +0900

    타입스크립트/C/ 공부

---
+++ TypeScript_learn/기본타입.ts
@@ -0,0 +1,26 @@
+// 기본타입
+
+// 논리
+let isRun:Boolean = false;
+
+// 숫자
+let decimal:number = 5;
+let hex:number = 0xff;
+
+// 문자열 
+let firstName:string = '김';
+let lastName:string = "철수";
+
+let fullName:string = `Full Name : ${lastName}  ${firstName}`   // 철수 김
+
+// 배열
+let list:number[] = [1,2,3]
+// 또는
+let list2:Array<number> = [1,2,3,4]
+
+// 튜플
+let point:[string, number]
+point = ["x", 10];
+point = [ 10, "x"]; // Error 
+
+
