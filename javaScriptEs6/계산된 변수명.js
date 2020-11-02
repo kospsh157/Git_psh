1fe23567ad7d5258af99454cf8943e4c494cba4a
kospsh157 <31397158+kospsh157@users.noreply.github.com>
2020-11-02 23:36:20 +0900

    정기 커밋

---
+++ javaScriptEs6/계산된 변수명.js
@@ -0,0 +1,15 @@
+// 프로퍼티의 계산된 이름 
+var person = {},
+    lastName = "last name";
+
+person['first name'] = 'Nicholas';
+person[lastName] = 'Zakas'; 
+
+// 위와 같이 속성명에 띄어쓰기를 할 수 있다. 
+// 기존 점을 이용한 접근법에서는 띄어쓰기가 되어 있는 속성명에는 사용하지 못했다.
+// 
+
+
+
+
+
